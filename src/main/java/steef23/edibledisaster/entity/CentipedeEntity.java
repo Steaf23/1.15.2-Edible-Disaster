d6ca5fd0d74d4d8454dac6c57c01257c5fbf6f9a
Steven van der Werf <59206556+steef23@users.noreply.github.com>
2020-12-25 03:48:11 +0100

    started on body part spawning, but is still recursivley broken :/

--- src/main/java/steef23/edibledisaster/entity/CentipedeEntity.java
+++ src/main/java/steef23/edibledisaster/entity/CentipedeEntity.java
@@ -3,34 +3,38 @@ package steef23.edibledisaster.entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import steef23.edibledisaster.init.EDEntityTypes;
 
 public class CentipedeEntity extends MobEntity
 {
 	private static final double defaultMoveSpeed = .2D;
 
-	private boolean isHead;
+	private boolean isHead = true;
+	private boolean firstTick = false;
 	
 	public CentipedeEntity(EntityType<? extends MobEntity> type, World worldIn) 
 	{
-		this(type, worldIn, true);
-
-	}
-
-	public CentipedeEntity(EntityType<? extends MobEntity> type, World worldIn, boolean isHead)
-	{
 		super(type, worldIn);
-		this.isHead = isHead;
-		
+
 	}
 	
 	@Override
 	public void tick() 
 	{
 		super.tick();
-		if (this.getHealth() < this.getMaxHealth())
+		
+		if (!this.world.isRemote())
 		{
-			this.isHead = false;
+			if (!firstTick)
+			{
+				firstTick = true;
+				if (isHead)
+				{
+					spawnBodyPart();
+				}
+			}
 		}
 	}
 	
@@ -43,7 +47,18 @@ public class CentipedeEntity extends MobEntity
 	    this.getAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(0.3D);
 	}
 	
-	public boolean isHead() {
+	private CentipedeEntity spawnBodyPart()
+	{
+		CentipedeEntity body = new CentipedeEntity(EDEntityTypes.CENTIPEDE_ENTITY.get(), this.world);
+		BlockPos pos = this.getPosition();
+		body.setPosition(pos.getX() + 1, pos.getY(), pos.getZ());
+		this.world.addEntity(body);	
+		return body;
+	}
+	
+	public boolean isHead() 
+	{
 		return isHead;
 	}
+	
 }
